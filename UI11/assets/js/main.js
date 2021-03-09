4632b4a8c8eef0930a02a51938e6158a7ccf481b
Francesco Luppi <francescoluppi@aol.com>
2021-03-09 11:39:22 +0100

    END: finito esercizio draggable

---
+++ UI11/assets/js/main.js
@@ -0,0 +1,40 @@
+// Prendi tutti gli elementi P con la classe .draggable
+const draggables = document.querySelectorAll('.draggable')
+//Prendi tutti gli elementi section che contengono gli elementi p
+const containers = document.querySelectorAll('section')
+
+draggables.forEach(draggable=>{
+    draggable.addEventListener('dragstart',()=>{
+        draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend',()=>{
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container=>{
+    container.addEventListener('dragover',(e)=>{
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if(afterElement == null){container.appendChild(draggable)}
+        else{
+            container.insertBefore(draggable, afterElement)
+        }
+    })
+})
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    return draggableElements.reduce((closest, child)=>{
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height/2
+        if(offset < 0 && offset > closest.offset){
+            return {offset: offset, element:child}
+        }else{
+            return closest
+        }
+    }, {offset: Number.POSITIVE_INFINITY}).element
+}
+
+
