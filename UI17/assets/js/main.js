f54bd39752632af980df17be849a99a720cc7a3e
frappaaa <francescoluppi@aol.com>
2021-03-25 14:59:35 +0100

    ADD: collegato correttamente luxon e applicato

--- UI17/assets/js/main.js
+++ UI17/assets/js/main.js
@@ -1,2 +1,20 @@
 let hours = document.querySelectorAll('.hour')
-let places = document.querySelectorAll('h2')
+
+const update = function(){
+    hours.forEach(hour => {
+        //Display orario diverso per la singola timezone
+        let timezone = hour.getAttribute('data-timezone')
+        let now = luxon.DateTime.now().setZone(timezone)
+        hour.textContent= now.toFormat('HH:mm:ss')
+
+        //colore diverso per fine della giornata
+        let timeDay= parseInt(now.toFormat('H'))
+        if(timeDay<9 || timeDay>18){
+            hour.style.color = 'red'
+        }
+    })
+}
+
+setInterval(function() {
+    update()
+}, 1000)
