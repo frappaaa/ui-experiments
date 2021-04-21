9082e0a7ce4f7db8c420b3d499f5a4c88763a19b
frappaaa <francescoluppi@aol.com>
2021-04-21 15:33:16 +0200

    ADD: figure create

--- UI21/js/main.js
+++ UI21/js/main.js
@@ -1,8 +1,27 @@
 console.clear();
 
+// Prima creo e elaboro questo
 function preload() {}
-function setup() {}
+
+//Poi parte questo
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  colorMode(RGB);
+}
 
 function draw() {
-  console.log("Hello");
+  background(255, 204, 0);
+  fill(0);
+
+  stroke("red");
+  strokeWeight(10);
+
+  translate(width / 2, height / 2);
+  rect(0, 0, 500, 500);
+  push();
+  fill("blue");
+  noStroke();
+  ellipse(0, 0, 50);
+  pop();
 }
