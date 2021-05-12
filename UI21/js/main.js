f999bf68d5c9672f5c40416e67115b17d7278b05
frappaaa <francescoluppi@aol.com>
2021-05-12 12:34:00 +0200

    ADD: use fft

--- UI21/js/main.js
+++ UI21/js/main.js
@@ -2,7 +2,7 @@ console.clear();
 let posX = 0,
   posY = 0;
 let audio = 0;
-let amp;
+let amp, fft;
 
 // Prima creo e elaboro questo
 function preload() {
@@ -13,23 +13,33 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  amp = new p5.Amplitude();
+
   audio.setVolume(0.1);
-  audio.pause();
+  audio.play();
+
+  amp = new p5.Amplitude();
+  fft = new p5.FFT();
 }
 
 function draw() {
   background(0);
   stroke(255);
-  translate(0, height / 2);
+  //translate(0, height / 2);
 
   const volume = amp.getLevel();
 
   // const mapW = map(volume, 0, 0.1, 0, 500);
   // rect(posX, posY, mapW, mapW);
 
-  const waveform = audio.getPeaks();
+  // const waveform = audio.getPeaks();
+  // for (let i = 0; i < waveform.length; i++) {
+  //   line(i, waveform[i] * 100, i, waveform[i] * -100);
+  // }
+
+  const waveform = fft.waveform();
   for (let i = 0; i < waveform.length; i++) {
-    line(i, waveform[i] * 100, i, waveform[i] * -10);
+    const x = map(i, 0, waveform.length, 0, width);
+    const y = map(waveform[i], -1, 1, 0, height);
+    point(x, y);
   }
 }
