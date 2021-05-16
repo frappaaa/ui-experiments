c94f6362c5cd87d750bca892b92850b1c286ab24
frappaaa <francescoluppi@aol.com>
2021-05-16 19:47:57 +0200

    ADD: first sketch

--- UI21/js/main.js
+++ UI21/js/main.js
@@ -1,11 +1,17 @@
-console.clear();
-let posX = 0,
-  posY = 0;
-let audio = 0;
-let amp,
+const bins = 16;
+let amplitude,
   fft,
-  bins = 1024,
-  binWidth;
+  waveAmp = 3,
+  lines;
+let volumeSlider;
+let beatDetect;
+let beatDetectHat;
+let bgColor = 0;
+let binColor;
+let binWidth;
+let spectrum;
+let volume;
+let progress;
 
 // Prima creo e elaboro questo
 function preload() {
@@ -14,21 +20,46 @@ function preload() {
 
 //Poi parte questo
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
-  canvas.mouseClicked(togglePlay);
-  fft = new p5.FFT(0, bins);
+  createCanvas(windowWidth, windowHeight);
+  // From our helpers.js
+  createControls();
+
+  // Sound analysis tools
+  fft = new p5.FFT(0.5, bins);
+  amplitude = new p5.Amplitude();
+
+  // Peak detection
+  beatDetect = new p5.PeakDetect(0, 20000, 0.035);
+  beatDetect.onPeak(triggerBeat);
+
+  beatDetectHat = new p5.PeakDetect(5200, 14000, 0.6);
+  beatDetectHat.onPeak(triggerBeatHat);
 
   binWidth = width / bins;
+  binColor = color(random(100));
 }
 
 function draw() {
-  background(0);
-  noStroke();
-  const spectrum = fft.analyze();
-  for (let i = 0; i < spectrum.length; i++) {
-    const y = map(spectrum[i], 0, 255, height, 0);
-    rect(i * binWidth, y, binWidth, height - y);
-  }
+  background(bgColor);
+  // Track's progress from 0 to 255
+  progress = map(audio.currentTime(), 0, audio.duration(), 255, 0);
+
+  //Start FFT
+  spectrum = fft.analyze();
+  beatDetect.update(fft);
+  beatDetectHat.update(fft);
+
+  // Volume Control
+  const level = amplitude.getLevel();
+  volume = map(level, 0, 0.2, 0, windowHeight / 2);
+  // Frequency Spectrum
+  if (audio.currentTime() > 8.6 && audio.currentTime() < 45) drawFFT();
+  // Volume Levels
+  if (audio.currentTime() > 45) drawRects();
+  // Mid Frequencies
+  if (audio.currentTime() > 26) drawCircle();
+  //Waveform
+  if (audio.currentTime() < 45) drawWaveform();
 }
 
 function togglePlay() {
@@ -37,4 +68,81 @@ function togglePlay() {
   } else {
     audio.play();
   }
+}
+
+function drawWaveform() {
+  const waveform = fft.waveform();
+  push();
+  noFill();
+  stroke("cyan");
+  beginShape();
+  for (let i = 0; i < waveform.length; i++) {
+    const x = map(i, 0, waveform.length, 0, width + 100);
+    const y = map(waveform[i], -0.2, 0.2, 0, height);
+    vertex(x, y);
+  }
+  endShape();
+  pop();
+}
+
+function drawFFT() {
+  noStroke();
+  for (let i = 0; i < spectrum.length; i++) {
+    let y = map(spectrum[i], 0, 255, height, 0);
+    push();
+    stroke(0);
+    strokeWeight(5);
+    fill(binColor);
+    rect(i * binWidth, y, binWidth, height - y);
+    pop();
+  }
+}
+
+function drawRects() {
+  const mapMouse = {
+    x: map(mouseX, 0, windowWidth, 2, 6),
+    y: map(mouseY, 0, windowHeight, 2, 6),
+  };
+  for (let i = 0; i <= windowWidth; i += windowWidth / mapMouse.x) {
+    for (let j = 0; j <= windowHeight; j += windowHeight / mapMouse.y) {
+      push();
+      noStroke();
+      fill(random(progress), random(progress), random(progress));
+      // rect(i, j, windowWidth, volume)
+      rect(i, j, windowWidth / 2, random(volume));
+      pop();
+    }
+  }
+}
+
+function drawCircle() {
+  const mid = fft.getEnergy("highMid");
+  const mapMid = map(mid, 100, 255, 0, windowHeight / 3);
+  push();
+  translate(windowWidth / 2, windowHeight / 2);
+  // fill(255)
+  fill(random(255));
+  circle(0, 0, mapMid);
+  pop();
+}
+
+function triggerBeat() {
+  if (audio.currentTime() > 45)
+    bgColor = color(random(255), random(255), random(255));
+}
+
+function triggerBeatHat() {
+  binColor = color(random(255), random(255), random(255), random(255));
+}
+
+function windowResized() {}
+
+function createControls() {
+  document.querySelector("button").addEventListener("click", () => {
+    if (audio.isPlaying()) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+  });
 }
